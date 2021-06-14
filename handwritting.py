e3aa55c4006e27d88587fec7b53dee5473242f37
yxshwxnt <79632155+yxshwxnt@users.noreply.github.com>
2021-06-15 01:04:38 +0530

    Add files via upload

---
+++ handwritting.py
@@ -0,0 +1,3 @@
+import pywhatkit 
+pywhatkit.text_to_handwriting('Whatever you want to convert.......',r'C:\Users\MY PC\Desktop\convert.png',rgb=[0,0,0])    
+# use [0,0,0] for black, [0,0,255] for blue and [0,255,0] for green   
